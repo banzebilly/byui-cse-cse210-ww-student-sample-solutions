2989baa9e659996b3b51973b37335b9d6aa098cd
banzebilly <bbanze49@gmail.com>
2026-09-01 13:34:24 +0200

    modified program file in the Exercise2 folder

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,45 +1,70 @@
-using System;
+// Author: Banze Billy
+// I m liking c# now my focus was on python and it frameworks
 
-class Program
+Console.Write("Enter your grade percentage: ");
+
+int grade = int.Parse(Console.ReadLine());
+
+string letter = "";
+
+if (grade >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.Write("What is your grade percentage? ");
-        string answer = Console.ReadLine();
-        int percent = int.Parse(answer);
+    letter = "A";
+}
+else if (grade >= 80)
+{
+    letter = "B";
+}
+else if (grade >= 70)
+{
+    letter = "C";
+}
+else if (grade >= 60)
+{
+    letter = "D";
+}
+else
+{
+    letter = "F";
+}
 
-        string letter = "";
+int determineGradeSign = grade % 10;
 
-        if (percent >= 90)
-        {
-            letter = "A";
-        }
-        else if (percent >= 80)
-        {
-            letter = "B";
-        }
-        else if (percent >= 70)
-        {
-            letter = "C";
-        }
-        else if (percent >= 60)
-        {
-            letter = "D";
-        }
-        else
-        {
-            letter = "F";
-        }
+string sign = "";
 
-        Console.WriteLine($"Your grade is: {letter}");
-        
-        if (percent >= 70)
-        {
-            Console.WriteLine("You passed!");
-        }
-        else
-        {
-            Console.WriteLine("Better luck next time!");
-        }
-    }
+if (determineGradeSign >= 7)
+{
+    sign = "+";
+}
+else if (determineGradeSign < 3)
+{
+    sign = "-";
+}
+else
+{
+    sign = "";
+}
+
+if (letter == "A" && sign == "+")
+{
+    sign = "";
+}
+else if (letter == "F" && sign == "+")
+{
+    sign = "";
+}
+else if (letter == "F" && sign == "-")
+{
+    sign = "";
+}
+
+Console.WriteLine($"Your grade is: {letter}{sign}");
+
+if (grade >= 70)
+{
+    Console.WriteLine("Congratulations! You passed the course.");
+}
+else
+{
+    Console.WriteLine("Keep working hard and try again next time.");
 }

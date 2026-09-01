2782ee35a3eeb29a1f19ddec9c29d2186e33dbeb
banzebilly <bbanze49@gmail.com>
2026-09-01 11:46:59 +0200

    first commit

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,15 +1,10 @@
-using System;
+//author: Banze
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
 
-        Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
+Console.Write("What is your first name? ");
+string firstName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last}, {first} {last}.");
-    }
-}
+Console.Write("What is your last name? ");
+string lastName = Console.ReadLine();
+
+Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");

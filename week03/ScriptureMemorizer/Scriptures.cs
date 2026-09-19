bb8e95cfe15aa7c7f58685bba9a61a838a2c13cb
banzebilly <bbanze49@gmail.com>
2026-09-19 11:37:18 +0200

    added Reference.cs,Scripture.cs, words.cs and modified Program.cs file

---
+++ week03/ScriptureMemorizer/Scriptures.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        foreach (string wordText in text.Split(' '))
+        {
+            _words.Add(new Word(wordText));
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string result = _reference.GetDisplayText() + "\n";
+
+        foreach (Word word in _words)
+        {
+            result += word.GetDisplayText() + " ";
+        }
+
+        return result.Trim();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int randomIndex = random.Next(_words.Count);
+
+            _words[randomIndex].Hide();
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
